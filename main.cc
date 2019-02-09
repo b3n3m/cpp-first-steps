7037652aa06effde8106d8993dc9f6e71d6321df
b3n3m <benem@web.de>
2019-02-09 21:40:22 +0100

    Add advanced cout and numeric limit for int

--- main.cc
+++ main.cc
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
-int main() { cout << "Hello, World!\n"; }
+int main() {
+  cout << "sizeof(int) = " << sizeof(int) * 8 << "bit" << endl
+       << "integer range = [" << numeric_limits<int>::min() << ", "
+       << numeric_limits<int>::max() << "]" << endl;
+
+  int x = 25;
+  cout << "Hello, World!\n"
+       << "Ich bin " << x << " Jahre jung." << endl;
+  // endl = schreibt Out vor Ende Quellcode und zeigt Vortschritt. Zwigeng wird
+  // Cache ausgegeben
+}
