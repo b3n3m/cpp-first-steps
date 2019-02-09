1c51343df53dfa78acdb38daab9f1163a6c6b301
b3n3m <benem@web.de>
2019-02-09 21:59:18 +0100

    Add input file example

---
+++ input.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello, Hello, Hello, Whats your name?\nEingabe: ";
+  string name;
+  cin >> name;
+  cout << "Ohh man Sonnenschein " << name << " ist wirklich ein schöner Name!"
+       << endl;
+}
