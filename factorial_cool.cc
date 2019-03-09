0d964152b6b7f1c5401b61892668291a984f116d
b3n3m <benem@web.de>
2019-03-09 13:50:20 +0100

    Add cooler factorial example

---
+++ factorial_cool.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    cout << "usage:" << '\n' << argv[0] << " <integer> [<integer>...]\n";
+    return -1;
+  }
+  for (int i = 1; i < argc; ++i) {
+    stringstream input{argv[i]};
+    int n;
+    input >> n;
+
+    int result = 1;
+    for (int i = 2; i <= n; ++i) result *= i;
+
+    cout << n << "! = " << result << '\n';
+  }
+}
