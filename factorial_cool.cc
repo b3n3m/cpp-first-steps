456f71c2b1b8594adbdbd4647593d1eec5b22bbd
b3n3m <benem@web.de>
2019-03-23 12:51:53 +0100

    Add declaritons for funktions  in factoria_cool

--- factorial_cool.cc
+++ factorial_cool.cc
@@ -4,21 +4,8 @@
 
 using namespace std;
 
-int factorial(int n) {
-  int result = 1;
-  for (int i = 2; i <= n; ++i) result *= i;
-  return result;
-}
-
-int to_int(char* text) {
-  stringstream input{text};
-  int n;
-  input >> n;
-  if (input.fail() || !input.eof()) {
-    throw invalid_argument(string("Argument '") + text + "' has wrong format!");
-  }
-  return n;
-}
+int factorial(int n);
+int to_int(char* text);
 
 int main(int argc, char** argv) {
   if (argc < 2) {
@@ -30,8 +17,26 @@ int main(int argc, char** argv) {
       const int n = to_int(argv[i]);
       const int result = factorial(n);
       cout << n << "! = " << result << '\n';
-    } catch (const exception& e) {
+    } catch (const invalid_argument& e) {
       cout << e.what() << '\n';
     }
   }
+}
+
+int factorial(int n) {
+  if (n < 0)
+    throw invalid_argument{"Cannot compute factorial of negative number!"};
+  int result = 1;
+  for (int i = 2; i <= n; ++i) result *= i;
+  return result;
+}
+
+int to_int(char* text) {
+  stringstream input{text};
+  int n;
+  input >> n;
+  if (input.fail() || !input.eof()) {
+    throw invalid_argument{string{"Argument '"} + text + "' has wrong format!"};
+  }
+  return n;
 }
